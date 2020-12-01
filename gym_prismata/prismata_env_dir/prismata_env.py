8726154c5c90f2a44405ed2b0fcd34f3d37707fc
Shauna Kravec <smkravec@celest.ai>
2020-12-01 11:00:39 -0800

    fixed p2 training bug

--- gym_prismata/prismata_env_dir/prismata_env.py
+++ gym_prismata/prismata_env_dir/prismata_env.py
@@ -2,11 +2,11 @@ import sys
 import gym
 from os import environ
 # Only works with POSIX-style paths
-environ["PRISMATA_INIT_AI_JSON_PATH"] = f"{'/'.join(__file__.split('/')[:-1])}/AI_config.txt"
+# environ["PRISMATA_INIT_AI_JSON_PATH"] = f"{'/'.join(__file__.split('/')[:-1])}/AI_config.txt"
 import prismataengine
 import random
 import numpy as np
-
+from time import sleep
 
 class PythonRandomPlayer(object):
     def getAction(self, gamestate):
@@ -39,33 +39,46 @@ class PrismataEnv(gym.Env):
         return obs, legal, reward, done, winner
     
     def getReward(self, obs, done): 
-        if __debug__:
-            print('Calculating reward...')
-        reward = np.dot(self.reward_hyper, obs)#Coefficient is an adjustable hyper
+        reward = np.dot(self.reward_hyper, obs)#Coefficients are an adjustable hyper
         winner=self.gamestate.winner()
-        if done and winner == prismataengine.Players.One:
+        if done and winner == self.NN_player:
             reward+=1 #another arbitrary hyper
-        elif done and winner == prismataengine.Players.Two:
-            reward-=1 
+        elif done and winner == self.opponent_player:
+            reward-=1
+        #if winner!=3:
+        #    print(winner)
         if __debug__:
             print(f'Reward calc successful ({reward})!')
         return reward
     
     def playOpponent(self, obs, legal, done):
-        if self.gamestate.activePlayer == prismataengine.Players.Two:
+        if self.gamestate.activePlayer == self.opponent_player:
             self.gamestate.step()
         return self.gamestate.toVector(), self.gamestate.getAbstractActionsVector(), self.gamestate.isGameOver()
     
-    def reset(self, policy = 'Random', cards='4'):
+    def reset(self, policy = 'Random', cards='4', NN_player='p1'):
         self.policy = policy
         self.cards = cards
+        self.NN_player = prismataengine.Players.One if NN_player == 'p1' else prismataengine.Players.Two
+        self.opponent_player = prismataengine.Players.Two if self.NN_player == prismataengine.Players.One else prismataengine.Players.One
         
-        if policy == 'Random':
-            self.player2 = PythonRandomPlayer()
-        elif type(policy) == object and hasattr(policy, 'getAction'):
-            self.player2 = self.policy
-        else:
-            self.player2 = self.policy
+        self.player1=None
+        self.player2=None
+        if self.NN_player == prismataengine.Players.One:
+            if policy == 'Random':
+                self.player2 = PythonRandomPlayer()
+            elif type(policy) == object and hasattr(policy, 'getAction'):
+                self.player2 = self.policy
+            else:
+                self.player2 = self.policy
+        elif self.NN_player == prismataengine.Players.Two:
+            if policy == 'Random':
+                self.player1 = PythonRandomPlayer()
+            elif type(policy) == object and hasattr(policy, 'getAction'):
+                self.player1 = self.policy
+            else:
+                self.player1 = self.policy
+                
         if cards == '4':
             self.gamestate = prismataengine.GameState('''{
                  "whiteMana":"0HH",
@@ -79,7 +92,7 @@ class PrismataEnv(gym.Env):
                      {"cardName":"Engineer", "color":1, "amount":2}
                  ],
                  "cards":["Drone","Engineer","Blastforge","Steelsplitter"]
-             }''',cards=4, player2=self.player2)
+             }''',cards=4, player1=self.player1, player2=self.player2)
             #self.reward_hyper=np.array([0, 0,
             #                         1,  0,  1,  2,
             #                         3,  3,  3,  2,  1,  2,  4,  5,  5,  6,
@@ -101,7 +114,7 @@ class PrismataEnv(gym.Env):
                      {"cardName":"Engineer", "color":1, "amount":2}
                  ],
                  "cards":["Drone","Engineer","Blastforge","Animus", "Conduit", "Steelsplitter", "Wall", "Rhino", "Tarsier", "Forcefield", "Gauss Cannon"]
-             }''',cards=11, player2=self.player2)
+             }''',cards=11, player1=self.player1, player2=self.player2)
             self.reward_hyper=np.zeros(82) #Same as above
             self.observation_space = np.ndarray(82)
             self.action_space_dim = 32
